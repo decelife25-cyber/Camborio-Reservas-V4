80b5e446ee0df4576c773d25a7f41997df4f69ef
decelife25-cyber <decelife25@gmail.com>
2026-09-05 17:12:34 +0200

    recovery: restaurar árbol del punto estable 45f95c8

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ window.CAMBORIO_CONFIG = Object.freeze({
   supabaseUrl: 'https://caeszgtogifserrxdrcw.supabase.co',
   supabaseAnonKey: 'sb_publishable_g8c6I-h5g0omPamXeduuYQ_V4g0PlZU',
   publicReservationsFunction: 'https://caeszgtogifserrxdrcw.supabase.co/functions/v1/public-reservas',
-  emailFunction: 'https://caeszgtogifserrxdrcw.supabase.co/functions/v1/send-camborio-email',
   timezone: 'Europe/Madrid'
 });
