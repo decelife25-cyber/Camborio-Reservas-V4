dc7543e83799913f2014de2964e3d722e8203d05
decelife25-cyber <decelife25@gmail.com>
2026-09-09 01:22:09 +0200

    chore: bump PWA build to 15

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CAMBORIO_CONFIG = Object.freeze({
-  appVersion: 'V4.0.0.14',
+  appVersion: 'V4.0.0.15',
   supabaseUrl: 'https://caeszgtogifserrxdrcw.supabase.co',
   supabaseAnonKey: 'sb_publishable_g8c6I-h5g0omPamXeduuYQ_V4g0PlZU',
   publicReservationsFunction: 'https://caeszgtogifserrxdrcw.supabase.co/functions/v1/public-reservas',
