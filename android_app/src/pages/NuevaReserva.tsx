efe348f6fcf1f2909b21786b2a88d601c64b5e17
decelife25-cyber <decelife25@gmail.com>
2026-09-22 04:44:02 +0200

    chore: limpiar import no utilizado en selector

--- android_app/src/pages/NuevaReserva.tsx
+++ android_app/src/pages/NuevaReserva.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { getTurnoFromHora } from '../utils/shifts';
 
