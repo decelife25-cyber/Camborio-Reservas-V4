58085e16b74c6ef4aba3445eaf097a92a78d5bca
decelife25-cyber <decelife25@gmail.com>
2026-09-25 10:58:33 +0200

    fix: correct MainActivity formatting

--- android_app/android/app/src/main/java/com/camborio/reservas/privada/MainActivity.java
+++ android_app/android/app/src/main/java/com/camborio/reservas/privada/MainActivity.java
@@ -10,7 +10,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {\n    public MainActivity() {\n        registerPlugin(BadgeNotificationPlugin.class);\n    }
+public class MainActivity extends BridgeActivity {
+    public MainActivity() {
+        registerPlugin(BadgeNotificationPlugin.class);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
